0a2cee1ba28eece75c7a660344f8111a11513f25
unknown <edumwas92@gmail.com>
2025-05-25 22:28:35 +0300

    Add Directors interface extending Teacher with numberOfReports property

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -19,3 +19,18 @@ interface Teacher {
   
   console.log(teacher3);
   
+  
+  interface Directors extends Teacher {
+    numberOfReports: number;
+  }
+  
+  // Example usage:
+  const director1: Directors = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+  };
+  
+  console.log(director1);
